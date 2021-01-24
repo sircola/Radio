dfb2dd57f356cb269915aac29bcf11bbe48d52b8
Yamakasi <Yamaksi@home>
2021-01-24 12:57:23 +0100

    mehet

--- Radio/Program.cs
+++ Radio/Program.cs
@@ -8,16 +8,17 @@ using System.IO;
 
 namespace Radio
 {    
-    struct Szam {
-        public int radio;
-        public int perc;
-        public int masodperc;
-        public string eloado;
-        public string cim;
-    }
-
     class Program
     {
+        class Szam
+        {
+            public int radio;
+            public int perc;
+            public int masodperc;
+            public string eloado;
+            public string cim;
+        }
+
         static void Main(string[] args)
         {
             List<Szam> szamok = new List<Szam>();
@@ -27,10 +28,7 @@ namespace Radio
             while (!sr.EndOfStream)
             {
                 string[] kettospont = sr.ReadLine().Split(':');
-                // for (int i = 0; i < kettospont.Length; i++) Console.WriteLine($"{i}: {kettospont[i]}");
-
                 string[] reszek = kettospont[0].Split(' ');
-                // for (int i = 0; i < reszek.Length; i++) Console.WriteLine($"{i}: {reszek[i]}");
 
                 Szam szam = new Szam();
 
@@ -40,53 +38,47 @@ namespace Radio
 
                 szam.eloado = "";
                 for (int i = 3; i < reszek.Length; i++)
-                {
-                    if (i > 3) szam.eloado += " ";
-                    szam.eloado += reszek[i];
-                }
+                    szam.eloado += reszek[i] + " ";
+                szam.eloado = szam.eloado.Trim();
 
                 reszek = kettospont[1].Split(' ');
                 szam.cim = "";
-                for (int i = 0; i < reszek.Length; i++)
-                {
-                    if (i > 0) szam.cim += " ";
-                    szam.cim += reszek[i];
-                }
+                foreach( string s in reszek )
+                    szam.cim += s + " ";
+                szam.cim = szam.cim.Trim();
 
                 szamok.Add(szam);
             }
             sr.Close();
-            // for (int i = 0; i < szamok.Count; i++) Console.WriteLine($"{i}:\n{szamok[i].radio}\n{szamok[i].perc}:{szamok[i].masodperc}\n{szamok[i].eloado}\n{szamok[i].cim}");
 
             Console.WriteLine("1. feladat");
             List<string> clapton = new List<string>();
-            for (int i = 0; i < szamok.Count; i++)
-                if (szamok[i].eloado == "Eric Clapton")
+            foreach( Szam szam in szamok )
+                if (szam.eloado == "Eric Clapton")
                 {
                     bool voltmar = false;
                     foreach (string s in clapton)
-                        if (s == szamok[i].cim)
+                        if (s == szam.cim)
                             voltmar = true;
                     if (voltmar == false)
-                        clapton.Add(szamok[i].cim);
+                        clapton.Add(szam.cim);
                 }
-            // foreach (string s in clapton) Console.WriteLine(s);
             Console.WriteLine($"{clapton.Count} darab Eric Clapton szam van.");
 
             Console.WriteLine("2. feladat");
-            int[] idok = new int[3];
+            int[] idok = { 0, 0, 0 };
 
-            for (int i = 0; i < szamok.Count; i++)
-                idok[szamok[i].radio - 1] += szamok[i].perc * 60 + szamok[i].masodperc;
+            foreach (Szam szam in szamok)
+                idok[szam.radio - 1] += szam.perc * 60 + szam.masodperc;
 
             for(int i=0; i<3; i++)
                 Console.WriteLine($"{i+1}. radio musorideje: {idok[i]/3600,0:D2}:{(idok[i] % 3600)/60,1:D2}:{idok[i]% 60,2:D2}");
 
             Console.WriteLine("3. feladat");
             int mennyi = 0;
-            foreach(Szam s in szamok)
-                foreach (Szam i in szamok)
-                    if (s.cim == i.cim && s.eloado == i.eloado && s.radio != i.radio)
+            foreach(Szam szam in szamok)
+                foreach (Szam s in szamok)
+                    if (szam.cim == s.cim && szam.eloado == s.eloado && szam.radio != s.radio)
                         ++mennyi;
             Console.WriteLine(mennyi > 1 ? "vannak" : mennyi == 1 ? "van" : "nincs");
 
